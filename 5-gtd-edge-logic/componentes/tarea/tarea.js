3e1900917af1240af717bf854dec09ad65b1a7a9
Alberto Basalo <albertobasalo@hotmail.com>
2016-01-21 14:25:21 +0100

    5 add

--- 5-gtd-edge-logic/componentes/tarea/tarea.js
+++ 5-gtd-edge-logic/componentes/tarea/tarea.js
@@ -2,21 +2,26 @@ import * as angular from 'angular'
 import * as angularMaterial from 'angular-material'
 import * as angularUiRouter from 'angular-ui-router'
 
-const raiz ="./componentes/"
+const raiz = "./componentes/"
 const nombreComponente = 'tarea'
 
 let configurarEstado = ($stateProvider) => {
-    $stateProvider
-        .state(nombreComponente, {
-            url: `/${nombreComponente}`,
-            template: `<${nombreComponente}></${nombreComponente}>`
-        });
+  $stateProvider
+    .state(nombreComponente, {
+      url: `/${nombreComponente}/:tareaId`,
+      template: `<${nombreComponente}></${nombreComponente}>`
+    });
 }
 
-angular.module(nombreComponente, ['ngMaterial','ui.router'])
-    .component(nombreComponente, {
-        templateUrl: `${raiz}${nombreComponente}/${nombreComponente}.html`
-    })
-    .config(configurarEstado)
+angular.module(nombreComponente, ['ngMaterial', 'ui.router'])
+  .component(nombreComponente, {
+    templateUrl: `${raiz}${nombreComponente}/${nombreComponente}.html`,
+    controller: function (servicioTareas, $stateParams) {
+      this.tareaId = $stateParams.tareaId
+      this.tarea = servicioTareas.obtenerTarea(this.tareaId)
+      this.guardar = servicioTareas.guardarTarea(this.tarea) 
+    }
+  })
+  .config(configurarEstado)
 
 export default nombreComponente
--- 5-gtd-edge-logic/servicioTareas.js
+++ 5-gtd-edge-logic/servicioTareas.js
@@ -8,21 +8,29 @@ function funcionTareas() {
 
   this.tareasIniciales = () => {
     this.tareas = [
-      { id: 1, texto: 'tarea inicial', estado: 'inbox' }, 
-      { id: 2, texto: 'tarea programada', estado: 'scheduled' }, 
-      { id: 3, texto: 'tarea ya hecha', estado: 'done' }]
+      { id: 1, titulo: 'tarea inicial', estado: 'inbox' },
+      { id: 2, titulo: 'tarea programada', estado: 'scheduled', fecha: new Date() },
+      { id: 3, titulo: 'tarea ya hecha', estado: 'done' }]
     this.tareasId = 3
   }
-
-  this.crearTarea = tarea => {
-    this.tareasId++
-    tarea.id = this.tareasId
-    tarea.estado = 'inbox'
-    this.tareas.push(tarea)
+  this.obtenerTarea = tareaId => {
+    return tareaId == 0 ? { id:0, estado: 'add' } : this.tareas.find(tarea => tarea.id == tareaId)
   }
 
-  this.borrarTarea = tareaID => {
-    let tareaIndex = this.tareas.findIndex(tarea => tarea.id == tareaID)
+  this.guardarTarea = tarea => {
+    if (tarea.id == 0) {
+      this.tareasId++
+      tarea.id = this.tareasId
+      tarea.estado = 'inbox'
+      this.tareas.push(tarea)
+    }
+    else{
+      this.cambiarTarea(tarea)
+    }
+  }
+
+  this.borrarTarea = tareaId => {
+    let tareaIndex = this.tareas.findIndex(tarea => tarea.id == tareaId)
     this.tareas.splice(tareaIndex, 1)
   }
 
@@ -30,7 +38,7 @@ function funcionTareas() {
     let tareaActual = this.tareas.find(tarea => tarea.id == tareaCambiada.id)
     tareaActual = tareaCambiada
   }
-  
+
   this.tareasIniciales()
 }
 
