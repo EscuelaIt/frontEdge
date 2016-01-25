4a1dc7d2650255a817ef75a0652578d98ade4d78
Alberto Basalo <albertobasalo@hotmail.com>
2016-01-25 16:43:15 +0100

    electron

---
+++ electron/main.js
@@ -0,0 +1,19 @@
+'use strict'
+let electron = require('electron');
+let app = electron.app;
+let BrowserWindow = electron.BrowserWindow;
+
+app.on('window-all-closed', ()=>{ 
+  if (process.platform != 'darwin') 
+    app.quit()
+  })
+
+app.on('ready', ()=> {
+    let mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    var webContents = mainWindow.webContents;
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.on('closed', ()=>  mainWindow = null);
+});
